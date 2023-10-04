968874a06bc58275e369a2822e02a56224cf4539
ailust <anonymous@email.git>
2023-10-03 19:45:13 -0700

    sdkjf

---
+++ class/constructor.cpp
@@ -0,0 +1,27 @@
+
+struct Struct {
+
+};
+
+class Class
+{
+public:
+    int member;
+
+    Class()
+    {
+        std::cout << "Class Constructor" << std::endl;
+        this->member = 3;
+    };
+
+    Class(int value) : member(value)
+};
+
+int main()
+{
+    Struct s;
+    Class c;
+    Class* p1;
+    Class* p2 = new Class();
+    return 0;
+}
